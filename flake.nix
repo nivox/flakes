7ee0fedd3cd56a4d5a3bc0f3001d2e77080a7021
Andrea Zito <zito.andrea@gmail.com>
2025-03-14 21:55:25 +0100

    added main flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Flake templates";
+
+  outputs = { self }: {
+    templates = {
+
+      rust = {
+        path = ./rust;
+        description = "Rust dev template";
+      };
+
+    }
+  }
+}
