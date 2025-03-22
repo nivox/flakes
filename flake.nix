c6cf3b75b6a82d4c310150f841994b33d917e721
Andrea Zito <zito.andrea@gmail.com>
2025-03-22 17:12:53 +0100

    added dev shell config

--- flake.nix
+++ flake.nix
@@ -1,7 +1,25 @@
 {
   description = "Flake templates";
 
-  outputs = { self }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells = let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in forEachSupportedSystem ({ pkgs }: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          nil
+          nixfmt-classic
+        ];
+      };
+    });
+
     templates = {
 
       rust = {
